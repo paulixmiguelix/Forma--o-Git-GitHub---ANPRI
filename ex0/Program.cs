b9c48b2ec943f966d68260f3ef0f9e350d1de293
Paulo Marques <4985614+paulixmiguelix@users.noreply.github.com>
2023-10-01 13:31:31 +0100

    Commit Inicial

---
+++ ex0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//inicio
+
+            //ex0
+            //nome:Prof
+            //04-10-2022
+            double x = 5.0;
+            Console.WriteLine(x);
+            Console.WriteLine(7.0);
+            Console.WriteLine("Bemvindo");
+
+            Console.Write("Introduza um número:");
+            x = Convert.ToDouble(Console.In.ReadLine());
+            Console.WriteLine("O nº que introduziu foi:"+x);
+
+            Console.In.ReadLine();// Para que o programa ñ encerre imediatamente
+
+
+
+
+
+
+
+        }//fim
+    }
+}
